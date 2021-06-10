775a691c02f91de2219de910969e1996fe7314e7
Jerry <jairosh.kumar6@gmail.com>
2021-06-10 23:59:42 +0530

    User Interactive

--- cats.py
+++ cats.py
@@ -5,11 +5,10 @@ from PIL import Image
 
 sq = input("Enter Search Query:")
 wb = input("Preferred browser:")
+output_file = input("Enter a title for the screenshot with the image extension:")
+
 webbrowser.get(wb).open('https://www.youtube.com/results?search_query='+sq)
 time.sleep(7)
-
 screenshot = pyautogui.screenshot()
-output_file = "Search Result.png"
 screenshot.save(output_file)
-
 screenshot.show()
