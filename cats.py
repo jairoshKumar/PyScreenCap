6e11491cbb2981829817d9491f9ecd362be4e101
avroshk <avroshkumar@gmail.com>
2021-06-10 14:28:53 -0400

    Adding Comments

--- cats.py
+++ cats.py
@@ -3,13 +3,21 @@ import pyautogui
 import time
 from PIL import Image
 
+# Variable
+wait_time = 7 # in seconds
+
+# Get user inputs
 sq = input("Enter Search Query:")
 wb = input("Preferred browser:")
-webbrowser.get(wb).open('https://www.youtube.com/results?search_query='+sq)
-time.sleep(7)
 
+# Open browser
+webbrowser.get(wb).open('https://www.youtube.com/results?search_query='+sq)
+
+# Wait
+time.sleep(wait_time)
+
+# Capture screen
 screenshot = pyautogui.screenshot()
 output_file = "Search Result.png"
 screenshot.save(output_file)
-
 screenshot.show()
