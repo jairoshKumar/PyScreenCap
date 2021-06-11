5181dfa1af57c6d81be45d3c7921784d8f2cab0c
Jerry <jairosh.kumar6@gmail.com>
2021-06-12 02:04:33 +0530

    Exceptions caught

--- cats.py
+++ cats.py
@@ -5,14 +5,27 @@ from PIL import Image
 
 # Variable
 wait_time = 7 # in seconds
+ok = False
 
 # Get user inputs
 sq = input("Enter Search Query:")
-wb = input("Preferred browser:")
+
+# File name input
 output_file = input("Enter a title for the screenshot with the image extension:")
 
 # Open browser
-webbrowser.get(wb).open('https://www.youtube.com/results?search_query='+sq)
+while (ok == False):
+    wb = input("\nPlease select the preferred browser:\nPress 1 for chrome\nPress 2 for safari\n")
+    if wb == '1':
+        webbrowser.get("chrome").open('https://www.youtube.com/results?search_query='+sq)
+        ok = True
+        break
+    elif wb == '2':
+        webbrowser.get("safari").open('https://www.youtube.com/results?search_query='+sq)
+        ok = True
+        break
+    else:
+        print("\nPLEASE ENTER CORRECT INPUT!!")
 
 # Wait
 time.sleep(wait_time)
